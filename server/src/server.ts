476f8f0a3a53bfd1a57c188eef41252014b717d1
Jozias Martini <jozias.martini@gmail.com>
2022-05-07 23:36:36 -0300

    Configure Prisma with SQLite and create Feedback table

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/users", (req, res) => {
+  return res.send("Hello world");
+});
+
 app.listen(3333, () => {
   console.log("HTTP server running!");
 });
