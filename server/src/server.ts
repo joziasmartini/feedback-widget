f8c7dbea307238acb6f2caf6539c719a993530bf
Jozias Martini <jozias.martini@gmail.com>
2022-05-08 01:13:58 -0300

    Create feedback route with Express and Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
+app.use(express.json());
 
-app.get("/users", (req, res) => {
-  return res.send("Hello world");
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
