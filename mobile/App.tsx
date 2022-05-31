ad592d64ac362180579c1899398399f6f0fa94e8
Jozias Martini <jozias.martini@gmail.com>
2022-05-31 18:56:50 -0300

    Update App component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,28 +1,44 @@
 import "react-native-gesture-handler";
-
-import AppLoading from "expo-app-loading";
-import { useFonts } from "@expo-google-fonts/inter";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
-import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
-
-import Widget from "./src/components/Widget";
+import { StyleSheet, View } from "react-native";
 import { theme } from "./src/theme";
+import Widget from "./src/components/Widget";
+import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Inter_400Regular,
     Inter_500Medium,
+    Inter_400Regular,
   });
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-
   return (
-    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
-      <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Widget />
-    </View>
+    <NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <StatusBar style="light" backgroundColor="transparent" translucent />
+          <Widget />
+        </View>
+      </GestureHandlerRootView>
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
